f564e3c3acf238daab20544aaea75f657bd42800
Leandro Escaldelai <leandro@massmedia.com.br>
2026-01-07 18:51:06 -0300

    kubernetes

---
+++ init-k8s.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+REM -- MongoDB
+kubectl apply -f mongo-prep-service.yaml
+kubectl apply -f mongo-prep-secrets.yaml
+kubectl apply -f mongo-prep.yaml
+
+REM -- App
+kubectl apply -f app-prep-service.yaml
+kubectl apply -f app-prep-ingress.yaml
+kubectl apply -f app-prep.yaml
